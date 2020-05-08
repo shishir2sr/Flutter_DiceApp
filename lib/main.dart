4e3aeed3d5fd2c2349461d9874d3322db0f8bd24
Yeasir Arefin TUsher <yasirarefinxz@gmail.com>
2020-05-08 22:28:13 +0600

    Final Dice App

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
